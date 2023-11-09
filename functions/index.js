599fd6ad62ee4c1f547508eb45dda4d9ec12f627
wood.wu <kilokingw@gmail.com>
2023-11-09 16:43:31 +0800

    wip funs0.0.1

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,16 @@ const { FieldValue } = require("firebase-admin/firestore");
 
 admin.initializeApp();
 
+exports.getDate = functions.https.onCall(() => {
+  var d = new Date(),
+	month = '' + (d.getMonth() + 1),
+	day = '' + d.getDate(),
+	year = d.getFullYear();
+
+	if (month.length < 2) 
+	month = '0' + month;
+	if (day.length < 2) 
+	day = '0' + day;
+	
+	return year+month+day+year+month+day
+});
