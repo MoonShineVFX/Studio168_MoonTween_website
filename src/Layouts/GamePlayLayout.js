2e1f251253d6e98de8bc810b9b1db6b48add5681
wood.wu <kilokingw@gmail.com>
2023-11-22 15:39:19 +0800

    wip patch whitearea0.0.1

--- src/Layouts/GamePlayLayout.js
+++ src/Layouts/GamePlayLayout.js
@@ -5,7 +5,7 @@ import { Outlet} from 'react-router-dom';
 function GamePlayLayout() {
   return (
     <div 
-      className='h-[100svh]  bg-black bg-center bg-no-repeat bg-cover relative'
+      className='h-[100vh]  bg-black bg-center bg-no-repeat bg-cover relative'
       style={{backgroundImage: `url(${'https://moonshine.b-cdn.net/msweb/studio168/bg.png'})`}}
     >
       <Outlet/>
