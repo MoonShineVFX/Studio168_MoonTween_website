b8698811aaaad03ce51fe42955e709890d1d533f
wood.wu <kilokingw@gmail.com>
2024-09-19 21:23:50 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,21 +5,21 @@ import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 // https://firebase.google.com/docs/emulator-suite/connect_firestore#android_apple_platforms_and_web_sdks
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
+import { getDatabase, INTERNAL as firebaseDbInternal } from "firebase/database";
 const firebase = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL:process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 const functions = getFunctions(firebase);
 const firestore = getFirestore(firebase);
 const database = getDatabase(firebase);
+firebaseDbInternal.forceLongPolling();
 // if (window.location.hostname === "localhost") {
 //   console.log(
 //     "Testing locally: hitting local functions and firestore emulators."
