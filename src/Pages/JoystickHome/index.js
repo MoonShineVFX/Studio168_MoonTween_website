9ca61f776199f441d514e4ab76e1be2a35440979
wood.wu <kilokingw@gmail.com>
2024-09-19 19:57:28 +0800

    Update index.js

--- src/Pages/JoystickHome/index.js
+++ src/Pages/JoystickHome/index.js
@@ -212,7 +212,7 @@ function Index({ title }) {
         //
         snapshot.forEach(function (childSnapshot) {
           var value = childSnapshot.val();
-          // console.log(value)
+          console.log(value);
           setCurrentUser(value);
 
           if (value.Status === "processing") {
